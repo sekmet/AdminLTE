0b02f468dda6e1dc5210fc927b2a0bc0e3af5f9d
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2015-08-12 01:00:12 +0200

    meteor fix

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 // package metadata file for Meteor.js
 var packageName = 'yp2:admin-lte';
 var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
-var version = '2.2.1_6';
+var version = '2.2.1_9';
 var summary = 'Admin-Lte meteor package';
-//var gitLink = 'https://github.com/yp2/AdminLTE';
+var gitLink = 'https://github.com/yp2/AdminLTE';
 var documentationFile = 'README.md';
 
 // Meta-data
@@ -11,7 +11,7 @@ Package.describe({
   name: packageName,
   version: version,
   summary: summary,
-  //git: gitLink,
+  git: gitLink,
   documentation: documentationFile
 });
 
@@ -21,8 +21,19 @@ Package.onUse(function(api) {
   api.use('jquery', where); // Dependencies
   api.use('twbs:bootstrap@3.3.5', where); // Dependencies
 
-  api.addFiles('index2.html', where); // Files in use
+
+  api.addFiles('index2.html', where);
+
   api.addFiles('dist/css/AdminLTE.css', where);
+  api.addFiles('dist/css/skins/_all-skins.css', where);
+
+  //api.addFiles('dist/css/skins/skin-black.css', where);
+  //api.addFiles('dist/css/skins/skin-blue.css', where);
+  //api.addFiles('dist/css/skins/skin-green.css', where);
+  //api.addFiles('dist/css/skins/skin-purple.css', where);
+  //api.addFiles('dist/css/skins/skin-red.css', where);
+  //api.addFiles('dist/css/skins/skin-yellow.css', where);
+
   api.addFiles('dist/js/app.js', where);
   api.addFiles('build/less/AdminLTE.less', where);
 
